89d9252660790ecb62ef8090bc78cf506ca4ac0a
egraessley <Egraessley07@yahoo.com>
2017-10-19 19:21:53 -0400

    added scripts for drill rotation

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    public int health;
+
+    // Use this for initialization
+    void Start()
+    {
+        switch(this.name)
+        {
+            case "Dirt":
+                health = 1;
+                break;
+
+           default:
+                break;
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void attack(float damage)
+    {
+        if(damage>health)
+        {
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Drill.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drill : MonoBehaviour {
+
+    public Drill S; // Singleton instance
+
+    //set in game engine
+    public float drillACC = .1f;        //acceleration of the drills angular momentum
+    public float drillDmgMult = .1f;    //how fast damage builds per fixed update    
+    public Color defaultColor = new Color(200, 200, 200, 255); //normal color of drill
+    public Color buildUp = new Color(200, 200, 200, 255);      //color of build up
+    public Color ideal = new Color(200, 200, 200, 255);         //final color before overheat
+    public Color overHeatColor = new Color(255, 150, 150, 255); //overheated color
+
+    public bool __________________________________________________________________________;
+    //values set dynamically
+    public GameObject[] leftDrills,rightDrills;
+    public GameObject target;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        S = this;
+
+        //so the drills rotate in opposite direction
+        leftDrills = GameObject.FindGameObjectsWithTag("Left");
+        rightDrills = GameObject.FindGameObjectsWithTag("Right");
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        foreach (GameObject drill in leftDrills)
+        {
+            drill.GetComponent<Rigidbody>().angularVelocity.Set(0, drill.GetComponent<Rigidbody>().angularVelocity.magnitude + drillACC * Time.deltaTime, 0);
+        }
+        //increase angular velocity of right 
+        foreach (GameObject drill in rightDrills)
+        {
+            drill.GetComponent<Rigidbody>().angularVelocity.Set(0, drill.GetComponent<Rigidbody>().angularVelocity.magnitude - drillACC * Time.deltaTime, 0);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetButtonUp("Down Arrow"))
+        {
+            //attack the block
+            //reset damage back to zero
+            //move piece
+        }
+       if(Input.GetButton("Down Arrow"))
+        {
+           //increase angular velocity of left drills
+           //increase angular velocity of right drills
+           //change colors incrementally
+        }
+    }
+}
