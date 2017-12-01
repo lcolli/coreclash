d86614a9b56cf238ad7859a59a516eafb3976e1f
Egraessley <Egraessley07@yahoo.com>
2017-11-30 20:51:18 -0500

    fixed so was the same as master

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+
+    public Vector3 destination;
+
+    private void Start()
+    {
+        destination = transform.position;
+    }
+
+	public void moveCam()
+    {
+        print("hit");
+        destination += new Vector3(0, -2, 0);
+    }
+
+    private void FixedUpdate()
+    {
+       transform.position = Vector3.Lerp(transform.position, destination , .02f);
+    } 
+}
---
+++ Assets/Scripts/PlayingField.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingField : MonoBehaviour
+{
+
+    [Header("Set in Inspector")]
+    public float maxHeight;
+    public float minHeight, leftAnchor;
+    public GameObject dirt, stone, metal, magma, water, treasure, gold, diamond,sand;
+    public Zone[] zones;
+
+    [Header("SetDynaimcally")]
+    
+    int level;
+    public bool done = false;
+
+
+    public void GameStart()
+    {
+        for(int i=0;i<zones.Length;i++)
+        {
+            placeZone(i);
+        }
+        done = true;
+    }
+
+
+    public void placeZone(int zoneNumber)
+    {
+        List<GameObject> zoneBlocks = new List<GameObject>();
+
+        for (int i=0;i<zones[zoneNumber].numDirt;i++)
+        {
+            zoneBlocks.Add(dirt);
+        }
+        for (int i = 0; i < zones[zoneNumber].numStone; i++)
+        {
+            zoneBlocks.Add(stone);
+        }
+        for (int i = 0; i < zones[zoneNumber].numMetal; i++)
+        {
+            zoneBlocks.Add(metal);
+        }
+        for (int i = 0; i < zones[zoneNumber].numMagma; i++)
+        {
+            zoneBlocks.Add(magma);
+        }
+        for (int i = 0; i < zones[zoneNumber].numWater; i++)
+        {
+            zoneBlocks.Add(water);
+        }
+        for (int i = 0; i < zones[zoneNumber].numTreasure; i++)
+        {
+            zoneBlocks.Add(treasure);
+        }
+        for (int i = 0; i < zones[zoneNumber].numGold; i++)
+        {
+            zoneBlocks.Add(gold);
+        }
+        for (int i = 0; i < zones[zoneNumber].numDiamond; i++)
+        {
+            zoneBlocks.Add(diamond);
+        }
+        for (int i = 0; i < zones[zoneNumber].numSand; i++)
+        {
+            zoneBlocks.Add(sand);
+        }
+
+        for (int i=0;i<9;i++)
+        {
+            int rand = Random.Range(0, zoneBlocks.Count-1);
+            GameObject toPlace=zoneBlocks[rand];
+            if(zoneNumber==0 && i<3 && toPlace.tag=="Treasure")     //treasure can't be on the surface
+            {
+                i--;
+            }
+            else
+            {
+                
+                float x, y, z=0;
+                x = leftAnchor + (i % 3)*2;
+                y = maxHeight - 2 * ((i / 3) + zoneNumber*3);
+                Vector3 placement = new Vector3(x, y, z);
+                Vector3 mirror = new Vector3(-x, y, z);                
+                GameObject blockR=Instantiate(toPlace,this.gameObject.transform);   
+                GameObject blockL=Instantiate(toPlace, this.gameObject.transform);
+                blockR.transform.position = placement;
+                blockL.transform.position = mirror;
+                zoneBlocks.RemoveAt(rand);
+            }
+        }
+
+    }
+}
+
+[System.Serializable]
+public class Zone
+{
+    public int numDirt, numStone, numMetal, numMagma, numWater, numTreasure, numGold, numDiamond,numSand;
+}
